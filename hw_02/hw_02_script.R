b0782381e69634d311ee8e96820ff23dd397852b
Philip Anderson <panders225@gmail.com>
2016-09-17 08:56:28 -0400

    completed 3.3 part a

--- hw_02/hw_02_script.R
+++ hw_02/hw_02_script.R
@@ -81,3 +81,52 @@ x <- seq(0,1,length.out = 1000)
 y <- dbeta(x = x, shape1 = 58, shape2 = 44)
 
 plot(x,y)
+
+
+# 3.3 part a
+
+y.a <- c(12,9,12,14,13,13,15,8,15,6)
+y.b <- c(11,11,10,9,9,8,7,10,6,8,8,9,7)
+
+plot(density(y.a), ylim=c(0,.25))
+lines(density(y.b), col="blue")
+
+# specify the parameters for population A
+a.a <- 120; a.b <- 10 # prior is y ~ gamma(120, 10)
+n.a <- length(y.a)
+sum.y.a <- sum(y.a)
+
+
+# the mean of the gamma distribution is given by (a / b)
+# which in this case will be: (sum.y.a + a.a) / (a.b + n.a)
+(sum.y.a + a.a) / (a.b + n.a)
+
+# the variance of the gamma distribution is given by (a / b^2)
+# which in this case will be: (sum.y.a + a.a) / (a.b + n.a)^2
+(sum.y.a + a.a) / (a.b + n.a)^2
+
+# The 95% quantile-based estimate will be found by pulling quantiles from the 
+# gamma distribution with the posterior parameters (sum.y.a + a.a) / (a.b + n.a)
+
+qgamma(c(0.025,0.975), (sum.y.a + a.a), (a.b + n.a) )
+
+
+# repeat this exercise for the independent B distribution
+
+b.a <- 12; b.b <- 1 # prior is y ~ gamma(12, 1)
+n.b <- length(y.b)
+sum.y.b <- sum(y.b)
+
+
+# the mean of the gamma distribution is given by (a / b)
+(sum.y.b + b.a) / (b.b + n.b)
+
+# the variance of the gamma distribution is given by (a / b^2)
+(sum.y.b + b.a) / (b.b + n.b)^2
+
+# The 95% quantile-based estimate will be found by pulling quantiles from the 
+# gamma distribution with the posterior parameters (a,b)
+
+qgamma(c(0.025,0.975), (sum.y.b + b.a), (b.b + n.b) )
+
+
