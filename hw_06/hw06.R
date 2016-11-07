7a7757941842fd0fd463b52c3d4804dc7f32e370
Philip Anderson <panders225@gmail.com>
2016-11-06 20:17:05 -0500

    hitting pause - set up second problem

--- hw_06/hw06.R
+++ hw_06/hw06.R
@@ -202,3 +202,135 @@ lines(density(COEF.ORANGE), lty=2, lwd = 2)
 legend(0.9, 30, c("Blue", "Orange"), lty=c(1,2), lwd=c(2,2))
 
 mean(COEF.BLUE < COEF.ORANGE)
+
+
+
+
+# additional problem
+
+#In addition, use the missing data program under the R Code link to 
+#analyze the bloodwork data (found under Data Sets).
+#This program has ample comments that will help you in running it.
+#Generate 10,000 observations from the posterior using this program.
+#Produce the same sort of summary of the output as I did in the 
+#class notes on pp. 168-172.  For each parameter, compute an 
+#approximate 95% credible interval in two ways.  
+#One way is to find the 2.5th and 97.5th percentiles 
+#of the generated parameter values.  Another way is to use 
+#mean +/- 1.96(sd).  Compare the intervals you get using the two 
+#different methods.
+
+bldwrk <- read.table("C:/Users/Philip/Schools/TAMU/STAT_638/Homework_Assignments/HW_06/bloodwork.txt")
+head(bldwrk, n = 5)
+
+missing=function(Y,theta,Sigma,nreps){
+  #
+  # For fun we'll save the generated values that replace the missing
+  # data.  We'll put them in an nreps x 2 matrix called output.   
+  #  
+  output=matrix(0,nreps,2)
+  Theta=matrix(0,nreps,3)
+  #
+  # Initialize the three-dimensional array where the values of Sigma will
+  # be saved.   
+  #  
+  Sigma.out=array(0,c(3,3,nreps))
+  n=nrow(Y)
+  #
+  # Specify the nonmissing data in the two rows that have missing data.   
+  #
+  y1=matrix(Y[42,1:2],2,1)
+  y2=matrix(Y[85,c(1,3)],2,1)
+  #
+  # Generate two observations that will be used in place of the missing
+  # values.  In doing so we use the starting parameters theta and Sigma. 
+  #
+  V=Sigma[1:2,1:2]
+  V=solve(V)
+  Cov=matrix(Sigma[3,1:2],1,2)
+  v=Sigma[3,3]
+  mu=theta[3]+as.vector(Cov %*% V %*% (y1-matrix(theta[1:2],2,1)))
+  var1=v-as.vector(Cov %*% V %*% t(Cov))
+  Y[42,3]=rnorm(1,mean=mu,sd=sqrt(var1))
+  V=Sigma[c(1,3),c(1,3)]
+  V=solve(V)
+  Cov=matrix(Sigma[2,c(1,3)],1,2)
+  v=Sigma[2,2]
+  mu=theta[2]+as.vector(Cov %*% V %*% (y2-matrix(theta[c(1,3)],2,1)))
+  var1=v-as.vector(Cov %*% V %*% t(Cov))
+  Y[85,2]=rnorm(1,mean=mu,sd=sqrt(var1))
+  theta=matrix(theta,1,3)
+  #
+  # Generate the nreps observations using Gibbs sampling.
+  #  
+  for(j in 1:nreps){
+    #
+    #  Compute the sample mean vector using the n x 3 "data" matrix that
+    #  includes the two most recently generated pseudo observations.
+    #
+    ybar=matrix(1,1,n)%*%Y/n
+    #
+    #   Generate theta given Y, Sigma and Ymiss, where Ymiss is a 2-vector
+    #   containing the current pseudo observations.  
+    #    
+    theta=rjmvnorm(1,as.vector(ybar),Sigma/n)
+    #
+    #   Compute the covariance matrix of the current n x 3 data matrix.
+    #    
+    S2=(n-1)*cov(Y)/n
+    #
+    #   Generate Sigma given Y, theta and Ymiss. 
+    #    
+    param=theta-ybar
+    param=n*S2+n*t(param)%*%param
+    param=solve(param)
+    Sigma=rjWishart(1,n,param)
+    Sigma=solve(Sigma[1:3,1:3,1])
+    #
+    #   Generate Ymiss given Y, theta and Sigma.  
+    #    
+    V=Sigma[1:2,1:2]
+    V=solve(V)
+    Cov=matrix(Sigma[3,1:2],1,2)
+    v=Sigma[3,3]
+    mu=theta[1,3]+as.vector(Cov %*% V %*% (y1-matrix(theta[1,1:2],2,1)))
+    var1=v-as.vector(Cov %*% V %*% t(Cov))
+    Y[42,3]=rnorm(1,mean=mu,sd=sqrt(var1))
+    V=Sigma[c(1,3),c(1,3)]
+    V=solve(V)
+    Cov=matrix(Sigma[2,c(1,3)],1,2)
+    v=Sigma[2,2]
+    mu=theta[1,2]+as.vector(Cov %*% V %*% (y2-matrix(theta[1,c(1,3)],2,1)))
+    var1=v-as.vector(Cov %*% V %*% t(Cov))
+    Y[85,2]=rnorm(1,mean=mu,sd=sqrt(var1))
+    output[j,]=c(Y[42,3],Y[85,2])
+    Theta[j,]=theta[1,1:3]
+    #
+    #   Note how we assign Sigma to the jth tier of Sigma.out.    
+    #
+    Sigma.out[1:3,1:3,j]=Sigma
+  }
+  #
+  # The three components of the output are put in a list.
+  #  
+  list(output,Theta,Sigma.out)
+}
+
+rjmvnorm=function(n,mu,Sigma){
+  p=length(mu)
+  X=matrix(rnorm(p*n),p,n)
+  Sigroot=t(chol(Sigma))
+  X=Sigroot %*% X + mu
+  t(X)
+}
+
+rjWishart=function(n,nu,M){
+  p=nrow(M)
+  out=array(0,c(p,p,n))
+  for(j in 1:n){
+    Z=rjmvnorm(nu,rep(0,len=p),M)
+    out[1:p,1:p,j]=t(Z)%*%Z
+  }
+  out
+}
+
